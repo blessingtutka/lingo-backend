210432eeba69527240df3194d31fc55de4024261
blessingtutka <blessingtutka298@gmail.com>
2025-07-23 15:07:24 +0200

    feat(middlewware): add auth and upload middlewares

---
+++ src/middlewares/authorize.ts
@@ -0,0 +1,18 @@
+import { Response, NextFunction } from 'express';
+import { Role } from '../config/prisma';
+import { AuthenticatedRequest } from '../interfaces/interfaces';
+
+const authorize = (role: Role) => {
+    return (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+        if (!req.user || !(role == req.user.role)) {
+            return res.status(403).json({
+                status: 'Forbidden',
+                message: 'You do not have permission to access this resource',
+                statusCode: 403,
+            });
+        }
+        next();
+    };
+};
+
+export default authorize;
