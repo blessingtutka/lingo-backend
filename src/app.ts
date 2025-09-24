5ea7a060d617643fc0526c5e8083e8de3ffc5b80
blessingtutka <blessingtutka298@gmail.com>
2025-09-24 12:19:10 +0200

    chore: update app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import path from 'path';
+import bodyParser from 'body-parser';
+import authRoutes from './modules/auth/auth.routes';
+import callRoutes from './modules/call/call.routes';
 
 const app: Application = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/call', callRoutes);
 
 //Not Found Routes
 app.use((req, res, next) => {
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 import app from './app';
 import config from './config';
+import { registerSocketEvents } from './events/socket';
 
 const port = config.port;
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+// app.listen(port, () => {
+//     console.log(`Server is running at http://localhost:${port}`);
+// });
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*',
+    },
+});
+
+registerSocketEvents(io);
+
+httpServer.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
